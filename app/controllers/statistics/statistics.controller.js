697c08ee610c83542f16602d37d92f6884e7caf9
MerakchiHibaa <ninamimi305@gmail.com>
2022-05-26 11:04:27 +0100

    feat(statistics) : version améliorée

--- app/controllers/statistics/statistics.controller.js
+++ app/controllers/statistics/statistics.controller.js
@@ -1,71 +1,194 @@
 const statisticsService = require("../../services/statistics/statistics.service")
-
-const getNbrDemandesAcceptSemaine = async (weekNumber , month , year ) => {
-    weekNumber = Number(weekNumber);
-    month = Number(month);
-    year = Number(year);
-    return statisticsService.getNbrDemandesAcceptSemaine(weekNumber, month, year) ;
+//TODO: REQ, RES after the test
+const getNbrDemandesAcceptSemaine = async (req , res ) => {
+    
+    weekNumber = Number(req.body.weekNumber);
+    month = Number(req.body.month);
+    year = Number(req.body.year);
+    const {code, data, error} = await statisticsService.getNbrDemandesAcceptSemaine(weekNumber, month, year) ;
+    if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
 }
 
-const getNbrDemandesRejetSemaine = async (weekNumber , month , year ) => {
-    weekNumber = Number(weekNumber);
-    month = Number(month);
-    year = Number(year);
-    return statisticsService.getNbrDemandesRejetSemaine(weekNumber, month, year)
+const getNbrDemandesRejetSemaine = async (req , res ) => {
+    weekNumber = Number(req.body.weekNumber);
+    month = Number(req.body.month);
+    year = Number(req.body.year);
+    const {code, data, error} = await statisticsService.getNbrDemandesRejetSemaine(weekNumber, month, year)
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
+}
+
+const getNbrDemandesAcceptMois = async (req , res) => {
+    month = Number(req.body.month);
+    year = Number(req.body.year);
+    const {code, data, error} = await statisticsService.getNbrDemandesAcceptMois(month, year) ;
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
+}
+
+const getNbrDemandesRejetMois = async (req , res ) => {
+    month = Number(req.body.month);
+    year = Number(req.body.year);
+    const {code, data, error} = await statisticsService.getNbrDemandesRejetMois(month, year) ;
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
+}
+
+const getNbrDemandesAcceptAnnee = async (req , res ) => {
+    year = Number(req.body.year);
+    const {code, data, error} = await statisticsService.getNbrDemandesAcceptAnnee(year) ;
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
+}
+
+const getNbrDemandesRejetAnnee = async (req , res ) => {
+    year = Number(req.body.year);
+    const {code, data, error} = await statisticsService.getNbrDemandesRejetAnnee(year);
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
+}
+
+
+
+const getNbrInscriptionsSemaine = async (req , res ) => {
+    weekNumber = Number(req.body.weekNumber);
+    month = Number(req.body.month);
+    year = Number(req.body.year);
+    const {code, data, error} = await statisticsService.getNbrInscriptionsSemaine(weekNumber, month, year) ;
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
+}
+
+const getNbrInscriptionsMois = async (req , res ) => {
+    month = Number(req.body.month);
+    year = Number(req.body.year);
+    const {code, data, error} = await getNbrInscriptionsMois(month, year);
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
 
 }
 
-const getNbrDemandesAcceptMois = async (month , year) => {
-    month = Number(month);
-    year = Number(year);
-    return statisticsService.getNbrDemandesAcceptMois(month, year) ;
-
-}
-
-const getNbrDemandesRejetMois = async (month , year) => {
-    month = Number(month);
-    year = Number(year);
-    return statisticsService.getNbrDemandesRejetMois(month, year) ;
-
-}
-
-const getNbrDemandesAcceptAnnee = async (year) => {
-    year = Number(year);
-    return statisticsService.getNbrDemandesAcceptAnnee(year) ;
-
-}
-
-const getNbrDemandesRejetAnnee = async (year) => {
-    year = Number(year);
-    return statisticsService.getNbrDemandesRejetAnnee(year);
+const getNbrInscriptionsAnnee = async (req , res ) => {
+    year = Number(req.body.year);
+    const {code, data, error} = await statisticsService.getNbrInscriptionsAnnee(year);
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
 
 }
 
 
+const getFinanceSemaine = async (req , res ) => {
+    weekNumber = Number(req.body.weekNumber);
+    month = Number(req.body.month);
+    year = Number(req.body.year);
+    
+    const {code, data, error} = await statisticsService.getFinanceSemaine(weekNumber, month, year) ;
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
+}
 
-const getNbrInscriptionsSemaine = async (weekNumber , month , year) => {
-    weekNumber = Number(weekNumber);
-    month = Number(month);
-    year = Number(year);
-    return statisticsService.getNbrInscriptionsSemaine(weekNumber, month, year) ;
+const getFinanceMois = async (req , res ) => {
+    month = Number(req.body.month);
+    year = Number(req.body.year);
+    
+    const {code, data, error} = await statisticsService.getFinanceMois(month, year) ;
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
 
 }
 
-const getNbrInscriptionsMois = async (month , year) => {
-    month = Number(month);
-    year = Number(year);
-    return getNbrInscriptionsMois(month, year);
-
-
+const getFinanceAnnee = async (req , res ) => {
+    year = Number(req.body.year);
+  
+    const {code, data, error} = await statisticsService.getFinanceAnnee(year) ;
+     if (!error){
+        // Send  message to user
+        res.status(code).json(data)
+        // Invoke logger
+    }else{
+        // Invoke error logger
+        console.log(error);
+        res.status(code).json(error)
+    }
 }
-
-const getNbrInscriptionsAnnee = async (year) => {
-    year = Number(year);
-    return statisticsService.getNbrInscriptionsAnnee(year);
-
-
-}
-
 module.exports = {
     getNbrDemandesAcceptAnnee , 
     getNbrDemandesAcceptMois , 
@@ -75,5 +198,8 @@ module.exports = {
     getNbrDemandesRejetSemaine , 
     getNbrInscriptionsAnnee , 
     getNbrInscriptionsMois , 
-    getNbrInscriptionsSemaine 
+    getNbrInscriptionsSemaine ,
+    getFinanceSemaine , 
+    getFinanceMois , 
+    getFinanceAnnee
 }
--- app/services/statistics/statistics.service.js
+++ app/services/statistics/statistics.service.js
@@ -3,78 +3,385 @@ const PrismaClient = require("@prisma/client").PrismaClient;
 const prisma = new PrismaClient();
 
 const getNbrDemandesAcceptSemaine = async (weekNumber , month , year ) => {
-    weekNumber = Number(weekNumber);
-    month = Number(month);
-    year = Number(year);
-    const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *,  DATEPART(week, date_reservation) AS WEEK,  EXTRACT(MONTH FROM date_reservation) AS MONTH, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE MONTH=${month} AND YEAR=${year} AND WEEK=${weekNumber} AND etat=COMPLETED OR etat=ENCOURS`
-    return result ;
+   
+    try {
+        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *,  DATEPART(week, date_reservation) AS WEEK,  EXTRACT(MONTH FROM date_reservation) AS MONTH, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE MONTH=${month} AND YEAR=${year} AND WEEK=${weekNumber} AND etat=COMPLETED OR etat=ENCOURS`
+    if(result) {
+        return {
+            code : 200 ,
+            data: {
+                success: true,
+                data: result
+
+            }
+        }
+    } 
+    else {
+        return {
+            code : 400 ,
+            data: {
+                success: false,
+                data: 0
+
+            }
+        }
+
+    }
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        } 
+
+    
+}
 }
 
 const getNbrDemandesRejetSemaine = async (weekNumber , month , year ) => {
-    weekNumber = Number(weekNumber);
-    month = Number(month);
-    year = Number(year);
-    const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *,  DATEPART(week, date_reservation) AS WEEK,  EXTRACT(MONTH FROM date_reservation) AS MONTH, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE MONTH=${month} AND YEAR=${year} AND WEEK=${weekNumber} AND etat=REJECTED`
-    return result ;
+   
+    try {
+        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *,  DATEPART(week, date_reservation) AS WEEK,  EXTRACT(MONTH FROM date_reservation) AS MONTH, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE MONTH=${month} AND YEAR=${year} AND WEEK=${weekNumber} AND etat=REJECTED`
+    if(result) {
+        return {
+            code : 200 ,
+            data: {
+                success: true,
+                data: result
 
+            }
+        }
+    } 
+    else {
+        return {
+            code : 400 ,
+            data: {
+                success: false,
+                data: 0
+
+            }
+        }
+
+    }
+
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+
+}
 }
 
 const getNbrDemandesAcceptMois = async (month , year) => {
-    month = Number(month);
-    year = Number(year);
-    const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(MONTH FROM date_reservation) AS MONTH, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE MONTH=${month} AND YEAR=${year} AND etat=COMPLETED OR etat=ENCOURS`
-    return result ;
+    
+    try {
+        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(MONTH FROM date_reservation) AS MONTH, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE MONTH=${month} AND YEAR=${year} AND etat=COMPLETED OR etat=ENCOURS`
+    if(result) {
+        return {
+            code : 200 ,
+            data: {
+                success: true,
+                data: result
 
+            }
+        }
+    } 
+    else {
+        return {
+            code : 400 ,
+            data: {
+                success: false,
+                data: 0
+
+            }
+        }
+
+    }
+
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+
+}
 }
 
 const getNbrDemandesRejetMois = async (month , year) => {
-    month = Number(month);
-    year = Number(year);
-    const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(MONTH FROM date_reservation) AS MONTH, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE MONTH=${month} AND YEAR=${year} AND etat=REJECTED`
-    return result ;
+   
+    try {
+        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(MONTH FROM date_reservation) AS MONTH, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE MONTH=${month} AND YEAR=${year} AND etat=REJECTED`
+    if(result) {
+        return {
+            code : 200 ,
+            data: {
+                success: true,
+                data: result
 
+            }
+        }
+    } 
+    else {
+        return {
+            code : 400 ,
+            data: {
+                success: false,
+                data: 0
+
+            }
+        }
+
+    }
+
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+    
+} 
 }
 
 const getNbrDemandesAcceptAnnee = async (year) => {
-    year = Number(year);
-    const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE YEAR=${year} AND etat=COMPLETED OR etat=ENCOURS`
-    return result ;
+    
+    try {
+        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE YEAR=${year} AND etat=COMPLETED OR etat=ENCOURS`
+    if(result) {
+        return {
+            code : 200 ,
+            data: {
+                success: true,
+                data: result
 
+            }
+        }
+    } 
+    else {
+        return {
+            code : 400 ,
+            data: {
+                success: false,
+                data: 0
+
+            }
+        }
+
+    }
+
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+    
+} 
 }
 
 const getNbrDemandesRejetAnnee = async (year) => {
-    year = Number(year);
-    const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE YEAR=${year} AND etat=REJECTED`
-    return result ;
+   
+    try {
+        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE YEAR=${year} AND etat=REJECTED`
+    if(result) {
+        return {
+            code : 200 ,
+            data: {
+                success: true,
+                data: result
 
+            }
+        }
+    } 
+    else {
+        return {
+            code : 400 ,
+            data: {
+                success: false,
+                data: 0
+
+            }
+        }
+
+    }
+
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+    
+} 
 }
 
 
 
 const getNbrInscriptionsSemaine = async (weekNumber , month , year) => {
-    weekNumber = Number(weekNumber);
-    month = Number(month);
-    year = Number(year);
-    const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *,  DATEPART(week, date_demande) AS WEEK,  EXTRACT(MONTH FROM date_demande) AS MONTH, EXTRACT(YEAR FROM date_demande) AS YEAR FROM DemandesInscription) WHERE MONTH=${month} AND YEAR=${year} AND WEEK=${weekNumber}`
-    return result ;
+    
+    try {
+        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *,  DATEPART(week, date_demande) AS WEEK,  EXTRACT(MONTH FROM date_demande) AS MONTH, EXTRACT(YEAR FROM date_demande) AS YEAR FROM DemandesInscription) WHERE MONTH=${month} AND YEAR=${year} AND WEEK=${weekNumber}`
+    if(result) {
+        return {
+            code : 200 ,
+            data: {
+                success: true,
+                data: result
 
+            }
+        }
+    } 
+    else {
+        return {
+            code : 400 ,
+            data: {
+                success: false,
+                data: 0
+
+            }
+        }
+
+    }
+
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+    
+} 
 }
 
 const getNbrInscriptionsMois = async (month , year) => {
-    month = Number(month);
-    year = Number(year);
-    const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(MONTH FROM date_demande) AS MONTH, EXTRACT(YEAR FROM date_demande) AS YEAR FROM DemandesInscription) WHERE MONTH=${month} AND YEAR=${year} `
-    return result ;
+    
+    try {
+        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(MONTH FROM date_demande) AS MONTH, EXTRACT(YEAR FROM date_demande) AS YEAR FROM DemandesInscription) WHERE MONTH=${month} AND YEAR=${year} `
+    if(result) {
+        return {
+            code : 200 ,
+            data: {
+                success: true,
+                data: result
+
+            }
+        }
+    } 
+    else {
+        return {
+            code : 400 ,
+            data: {
+                success: false,
+                data: 0
+
+            }
+        }
+
+    }
 
 
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+    
+} 
 }
 
 const getNbrInscriptionsAnnee = async (year) => {
-    year = Number(year);
-    const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(YEAR FROM date_demande) AS YEAR FROM DemandesInscription) WHERE YEAR=${year} `
+  
+    try {
+        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(YEAR FROM date_demande) AS YEAR FROM DemandesInscription) WHERE YEAR=${year} `
+    if(result) {
+        return {
+            code : 200 ,
+            data: {
+                success: true,
+                data: result
+
+            }
+        }
+    } 
+    else {
+        return {
+            code : 400 ,
+            data: {
+                success: false,
+                data: 0
+
+            }
+        }
+
+    }
+
+
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+    
+} 
+}
+
+
+
+
+
+const getFinanceSemaine = async (weekNumber , month , year) => {
+    
+    try {
+        const result = await prisma.$queryRaw`SELECT sum(real_price) FROM  (SELECT *,  DATEPART(week, date_paiment) AS WEEK,  EXTRACT(MONTH FROM date_paiment) AS MONTH, EXTRACT(YEAR FROM date_paiment) AS YEAR FROM Paiment) WHERE MONTH=${month} AND YEAR=${year} AND WEEK=${weekNumber}`
+    return result ;
+
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+    
+} 
+}
+
+const getFinanceMois = async (month , year) => {
+    
+    try {
+        const result = await prisma.$queryRaw`SELECT sum(real_price) FROM  (SELECT *, EXTRACT(MONTH FROM date_paiment) AS MONTH, EXTRACT(YEAR FROM date_paiment) AS YEAR FROM Paiment) WHERE MONTH=${month} AND YEAR=${year} `
     return result ;
 
 
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+    
+}
+}
+
+const getFinanceAnnee = async (year) => {
+  
+    try {
+        const result = await prisma.$queryRaw`SELECT sum(real_price) FROM  (SELECT *, EXTRACT(YEAR FROM date_paiment) AS YEAR FROM Paiment) WHERE YEAR=${year} `
+    return result ;
+
+
+} catch(e) {
+    return {
+            code: 500,
+            data: `Server error ${e.meta.cause}`,
+            error: e
+        }
+    
+}
 }
 
 module.exports = {
@@ -86,5 +393,8 @@ module.exports = {
     getNbrDemandesRejetSemaine , 
     getNbrInscriptionsAnnee , 
     getNbrInscriptionsMois , 
-    getNbrInscriptionsSemaine 
+    getNbrInscriptionsSemaine ,
+    getFinanceSemaine , 
+    getFinanceMois , 
+    getFinanceAnnee
 }
