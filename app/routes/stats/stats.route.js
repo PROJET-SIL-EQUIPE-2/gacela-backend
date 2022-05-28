be1e57adc6d33101693d881d634467773806fa7f
MohamedGouaouri <im_gouaouri@esi.dz>
2022-05-28 16:38:52 +0100

    add payment stats

--- app/routes/stats/stats.route.js
+++ app/routes/stats/stats.route.js
@@ -1,10 +1,16 @@
 const router = require("express").Router()
 const rentalsStatsController = require("../../controllers/stats/rentals.controller")
+const paymentStatsController = require("../../controllers/stats/payments.controller")
 /**
  * Rate of usage based on number of rentals per region
  * */
 router.get("/rental", rentalsStatsController.rental)
 
+router.get("/payment/yearly", paymentStatsController.yearlyReport)
+
+router.get("/payment/monthly", paymentStatsController.monthlyReport)
+
+router.get("/payment/daily", paymentStatsController.dailyReport)
 
 
 module.exports = router
