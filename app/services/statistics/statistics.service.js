319bed5bdfed9180644d67f4741a67df1a20d62f
MerakchiHibaa <ninamimi305@gmail.com>
2022-06-02 10:40:25 +0100

    fix(nbr inscription par semaine) fixed it

--- app/services/statistics/statistics.service.js
+++ app/services/statistics/statistics.service.js
@@ -1,5 +1,8 @@
 const PrismaClient = require("@prisma/client").PrismaClient;
 
+const { Client } = require('pg')
+
+
 const prisma = new PrismaClient();
 
 const getNbrDemandesAcceptSemaine = async (weekNumber , month , year ) => {
@@ -146,114 +149,68 @@ const getNbrDemandesRejetMois = async (month , year) => {
 } 
 }
 
+
+
+
 const getNbrDemandesAcceptAnnee = async (year) => {
-    
-    try {
-        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE YEAR=${year} AND etat=COMPLETED OR etat=ENCOURS`
-    if(result) {
+    const client = new Client()
+    await client.connect()
+    console.log(year)
+    const result = await client.query(`SELECT count(*) as TOTAL FROM learn.public."Reservations" WHERE EXTRACT(YEAR FROM date_reservation)=${year} AND etat=COMPLETED OR etat=ENCOURS;`)
+    await client.end()
         return {
             code : 200 ,
             data: {
                 success: true,
+                year : year ,
                 data: result
 
             }
         }
-    } 
-    else {
-        return {
-            code : 400 ,
-            data: {
-                success: false,
-                data: 0
-
-            }
-        }
-
-    }
-
-} catch(e) {
-    return {
-            code: 500,
-            data: `Server error ${e.meta.cause}`,
-            error: e
-        }
     
-} 
+
+
+
 }
 
 const getNbrDemandesRejetAnnee = async (year) => {
    
-    try {
-        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *, EXTRACT(YEAR FROM date_reservation) AS YEAR FROM Reservations) WHERE YEAR=${year} AND etat=REJECTED`
-    if(result) {
+    const client = new Client()
+    await client.connect()
+    console.log(year)
+    const result = await client.query(`SELECT count(*) as TOTAL FROM learn.public."Reservations" WHERE EXTRACT(YEAR FROM date_reservation)=${year} AND etat=REJECTED;`)
+    await client.end()
         return {
             code : 200 ,
             data: {
                 success: true,
+                year : year ,
                 data: result
 
             }
         }
-    } 
-    else {
-        return {
-            code : 400 ,
-            data: {
-                success: false,
-                data: 0
-
-            }
-        }
-
-    }
-
-} catch(e) {
-    return {
-            code: 500,
-            data: `Server error ${e.meta.cause}`,
-            error: e
-        }
     
-} 
 }
 
 
 
 const getNbrInscriptionsSemaine = async (weekNumber , month , year) => {
     
-    try {
-        const result = await prisma.$queryRaw`SELECT count(*) FROM  (SELECT *,  DATEPART(week, date_demande) AS WEEK,  EXTRACT(MONTH FROM date_demande) AS MONTH, EXTRACT(YEAR FROM date_demande) AS YEAR FROM DemandesInscription) WHERE MONTH=${month} AND YEAR=${year} AND WEEK=${weekNumber}`
-    if(result) {
-        return {
-            code : 200 ,
-            data: {
-                success: true,
-                data: result
-
-            }
-        }
-    } 
-    else {
-        return {
-            code : 400 ,
-            data: {
-                success: false,
-                data: 0
-
-            }
-        }
-
-    }
-
-} catch(e) {
-    return {
-            code: 500,
-            data: `Server error ${e.meta.cause}`,
-            error: e
-        }
+            const client = new Client()
+        await client.connect()
+        console.log(year)
+        const result = await client.query(`SELECT count(*) as TOTAL FROM learn.public."DemandesInscription" WHERE EXTRACT(YEAR FROM date_demande)=${year} AND EXTRACT(MONTH FROM date_demande)=${month} AND DATEPART(week, date_demande)= ${weekNumber} ;`)
+        await client.end()
+            return {
+                code : 200 ,
+                data: {
+                    success: true,
+                    year : year ,
+                    data: result
     
-} 
+                }
+            }
+        
 }
 
 const getNbrInscriptionsMois = async (month , year) => {
