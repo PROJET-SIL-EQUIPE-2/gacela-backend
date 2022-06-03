bd517c1aa85f3cde4dc10f378a7446bcaaca93f4
namgalff <96527743+namgalff@users.noreply.github.com>
2022-06-03 11:49:21 +0200

    Update agents.service.js

--- app/services/agents/agents.service.js
+++ app/services/agents/agents.service.js
@@ -2,75 +2,79 @@ const PrismaClient = require("@prisma/client").PrismaClient;
 
 const prisma = new PrismaClient();
 
-
 const getAllAgents = async () => {
-    try {
-        const allAgents = await prisma.AgentsMaintenance.findMany({
-            select: {
-                name: true,
-                family_name: true,
-                email: true,
-                phone_number: true,
-                blocked: true
-
-            }
-        });
-        return {
-            code: 200,
-            data: {
-                success: true,
-                data: allAgents
-
-            },
-        }
-    }catch (e){
-        return {
-            code: 500,
-            data: `Server error, ${e.meta.cause}`,
-            log: `Server error, ${e.meta.cause}`,
-            serviceError: e
-        }
-    }
-}
+  try {
+    const allAgents = await prisma.AgentsMaintenance.findMany({
+      select: {
+        name: true,
+        family_name: true,
+        email: true,
+        phone_number: true,
+        blocked: true,
+      },
+    });
+    return {
+      code: 200,
+      data: {
+        success: true,
+        data: allAgents,
+      },
+    };
+  } catch (e) {
+    return {
+      code: 500,
+      data: `Server error, ${e.meta.cause}`,
+      log: `Server error, ${e.meta.cause}`,
+      serviceError: e,
+    };
+  }
+};
 
 const getById = async (id) => {
-    try {
-        const agent = await prisma.AgentsMaintenance.findUnique({
-            where: {
-                agent_id: id
-            }
-        })
-        if (agent){
-            return {
-                code: 200,
-                data: {
-                    success: true,
-                    data: {
-                        agent
-                    }
-                }
-            }
-        }
-        else{
-            return {
-                code: 400,
-                data: {
-                    success: false,
-                    data: `No agent with id ${id} that was found`
-                }
-            }
-        }
-    }catch (e) {
-        return {
-            code: 500,
-            data: `Server error, ${e.meta.cause}`,
-            log: `Server error, ${e.meta.cause}`,
-            serviceError: e
-        }
+  try {
+    const agent = await prisma.AgentsMaintenance.findUnique({
+      select: {
+        name: true,
+        family_name: true,
+        email: true,
+        phone_number: true,
+        blocked: true,
+        Vehicules: true,
+      },
+      where: {
+        agent_id: id,
+      },
+    });
+    if (agent) {
+      return {
+        code: 200,
+        data: {
+          success: true,
+          data: {
+            agent,
+          },
+        },
+      };
+    } else {
+      return {
+        code: 400,
+        data: {
+          success: false,
+          data: `No agent with id ${id} that was found`,
+        },
+      };
     }
-}
+  } catch (e) {
+    return {
+      code: 500,
+      data: `Server error, ${e.meta.cause}`,
+      log: `Server error, ${e.meta.cause}`,
+      serviceError: e,
+    };
+  }
+};
 
 module.exports = {
-    getAllAgents,
-    getById
-}
+  getAllAgents,
+  getById,
+};
