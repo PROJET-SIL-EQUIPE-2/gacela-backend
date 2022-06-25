9b4798e7929de2b214a4ff71d83a6c62fd51fced
namgalff <96527743+namgalff@users.noreply.github.com>
2022-06-25 15:46:04 +0200

    c

--- app/routes/auth/signup.router.js
+++ app/routes/auth/signup.router.js
@@ -3,34 +3,51 @@ const signUpController = require("../../controllers/auth/signup.controller");
 const auth = require("../../middlewares/auth/authorize");
 const Role = require("../../middlewares/auth/roles");
 
-const multer  = require('multer')
+const multer = require("multer");
 const path = require("path");
 const uploadPath = "images/locataires/";
 
-const uploader = multer({ dest: path.join("uploads", uploadPath) })
+const uploader = multer({ dest: path.join("uploads", uploadPath) });
 
 // register new locataire route
-router.post("/locataire", uploader.fields([
-        { name: 'personal_photo', maxCount: 1 },
-        { name: 'photo_identity', maxCount: 1 }
-    ]
-), signUpController.signUpLocataire);
-
+router.post(
+  "/locataire",
+  uploader.fields([
+    { name: "personal_photo", maxCount: 1 },
+    { name: "photo_identity", maxCount: 1 },
+  ]),
+  signUpController.signUpLocataire
+);
 
 // validate locataire demand by email
-router.post("/locataire/validate", auth.authorize(Role.Admin), signUpController.validateLocataire);
+router.post(
+  "/locataire/validate",
+  auth.authorize(Role.Admin),
+  signUpController.validateLocataire
+);
 
 // reject locataire and send him a justification
-router.post("/locataire/reject", auth.authorize(Role.Admin), signUpController.rejectLocataire);
-
+router.post(
+  "/locataire/reject",
+  auth.authorize(Role.Admin),
+  signUpController.rejectLocataire
+);
 
 // Register a new agent route
-router.post("/agent", auth.authorize([Role.Admin, Role.Decideur]),signUpController.signUpAM);
+router.post("/agent", signUpController.signUpAM);
 
 // Register new admin
-router.post("/admin", auth.authorize([Role.Admin]),signUpController.registerAdmin);
+router.post(
+  "/admin",
+  auth.authorize([Role.Admin]),
+  signUpController.registerAdmin
+);
 
 // Register a new dicedeur
-router.post("/decideur", auth.authorize([Role.Admin]),signUpController.registerDicedeur);
+router.post(
+  "/decideur",
+  auth.authorize([Role.Admin]),
+  signUpController.registerDicedeur
+);
 
 module.exports = router;
